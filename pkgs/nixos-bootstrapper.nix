ab51309cca09f44ca4b5d07e46f1e4a0bd52f964
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-06-28 00:42:45 +0500

    Refactor installPhase to simplify binary search and improve ISO upload process

--- pkgs/nixos-bootstrapper.nix
+++ pkgs/nixos-bootstrapper.nix
@@ -6,27 +6,17 @@ stdenv.mkDerivation {
   
   src = nixos-bootstrapper-src;
 
-  # Работаем в корне директории распаковки
   sourceRoot = ".";
 
   installPhase = ''
     mkdir -p $out/bin
-    
-    echo "=================================================="
-    echo "СТРУКТУРА РАСПАКОВАННОГО АРХИВА В ПЕСОЧНИЦЕ NIX:"
-    echo "=================================================="
     ls -R
-    echo "=================================================="
-
-    # Временный гибкий поиск, чтобы билд прошел в любом случае
     TARGET_BIN=$(find . -type f -name "*nixos-bootstrapper*" | head -n 1)
     
     if [ -z "$TARGET_BIN" ]; then
-      echo "Ошибка: Не удалось найти файл по маске *nixos-bootstrapper*!"
       exit 1
     fi
     
-    echo "Найден бинарник: $TARGET_BIN -> Копируем в $out/bin/nixos-bootstrapper"
     cp "$TARGET_BIN" $out/bin/nixos-bootstrapper
     chmod +x $out/bin/nixos-bootstrapper
   '';
