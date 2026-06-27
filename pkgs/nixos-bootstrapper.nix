a2cae436949b22885f8bf1e621d650a32ff394e4
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-06-28 00:14:56 +0500

    Update nixos-bootstrapper version and simplify installPhase

--- pkgs/nixos-bootstrapper.nix
+++ pkgs/nixos-bootstrapper.nix
@@ -2,26 +2,14 @@
 
 stdenv.mkDerivation {
   pname = "nixos-bootstrapper";
-  version = "0.1.0";
+  version = "0.1.1"; 
   
   src = nixos-bootstrapper-src;
-
   sourceRoot = ".";
 
   installPhase = ''
     mkdir -p $out/bin
-    
-    TARGET_BIN=$(find . -type f -name "nixos-bootstrapper*" | head -n 1)
-    
-    if [ -z "$TARGET_BIN" ]; then
-      echo "Error: Cannot find any file matching 'nixos-bootstrapper*' in the archive!"
-      ls -R
-      exit 1
-    fi
-    
-    echo "Found binary at $TARGET_BIN, copying to $out/bin/nixos-bootstrapper"
-    cp "$TARGET_BIN" $out/bin/nixos-bootstrapper
-    
+    cp nixos-bootstrapper $out/bin/
     chmod +x $out/bin/nixos-bootstrapper
   '';
 }
