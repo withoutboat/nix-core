2956a7d0a80615fbf51a95c2adfee4203eb17bcf
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-07-10 16:27:12 +0500

    Update nixos-bootstrapper to version 0.1.15 and fix sha256 checksum

--- pkgs/nixos-bootstrapper.nix
+++ pkgs/nixos-bootstrapper.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, lib }:
 
 let
-  version = "0.1.14";
+  version = "0.1.15";
 in
 stdenv.mkDerivation {
   pname = "nixos-bootstrapper";
@@ -9,7 +9,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://github.com/withoutboat/nixos-bootstrapper/releases/download/v${version}/nixos-bootstrapper-linux-amd64.tar.gz";
-    sha256 = "sha256-mrAlmLky4IvfMHGiMAhwqQ+1rW8fNOKnNUFMC0zghkY="; 
+    sha256 = "sha256-dur7ovMNdPNMNf5x+myMZSGCNmxHX+ywoucejnOBn84="; 
   };
 
   sourceRoot = ".";
