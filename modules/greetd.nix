e5b57294d4328e63c51e0b6850798c9994d4b479
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-07-13 14:03:22 +0500

    feat: add greetd service configuration with default session settings

---
+++ modules/greetd.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+{
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-user --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal";
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
+}
