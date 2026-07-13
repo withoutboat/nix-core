2af31f77bde4e59abc85cbfea2ecf3975f44a723
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-07-13 17:13:38 +0500

    fix(greetd): ensure StandardInput and StandardOutput are set to tty

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -23,9 +23,10 @@ in
 
   systemd.services.greetd.serviceConfig = {
     Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
     StandardError = "journal";
     TTYReset = true;
     TTYVHangup = true;
     TTYVTDisallocate = true;
-  };
-}
+  };}
