4c89c94a677a188cb4f3afaafe915bab2e0b9bdc
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-07-14 19:05:50 +0500

    fix(hyperland): uncomment Hyprland program configuration for proper functionality

--- modules/hyperland.nix
+++ modules/hyperland.nix
@@ -7,10 +7,10 @@
     NIXOS_OZONE_WL = "1";
   };
 
- # programs.hyprland = {
- #   enable = true;
- #   package = inputs.hyprland.packages.${pkgs.system}.hyprland;
- #   portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
- #  # xwayland.enable = true;
- # };
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+    xwayland.enable = true;
+  };
 }
