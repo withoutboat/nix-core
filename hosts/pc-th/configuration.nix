a94af31b1c03593114523fad65900e33aa6e3f1c
withoutboat <withoutboat@gmail.com>
2026-09-06 07:47:22 +0000

    feat: add config

---
+++ hosts/pc-th/configuration.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  _module.args.spec = {
+    username = "withoutboat";
+    cpu = "intel";
+    gpu = "hybrid-amd-nvidia";
+    nvidiaOpen = true;
+    wifiSSID = "Xiaomi_89EF";
+    wifiPass = "21285090";
+    intelBusId = "";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+}
